944562a7dc0e758ec1177de2cd4329be1913b818
Chad Smith <csmith@Chads-MacBook-Pro.local>
2021-07-19 20:31:32 -0600

    demo updates

---
+++ 10_s3_operations/create_s3_bucket.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Set all the variables
+BUCKET1=$1
+REGION=$3
+
+#Create the two buckets
+aws s3api create-bucket --bucket $BUCKET1 --region $REGION
+
+#Enable bucket versioning on both buckets
+aws s3api put-bucket-versioning --bucket $BUCKET1 --versioning-configuration Status=Enabled --region $REGION
+
